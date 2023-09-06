c34f601f6bd8849ac7424632db0279fd61722152
unknown <ipanirtiano@gmail.com>
2023-09-07 00:46:41 +0700

    instal mysql2 module

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
+import mysql2 from "mysql2";
 
 const db = new Sequelize({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   dialect: "mysql",
+  dialectModule: mysql2,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
 });
